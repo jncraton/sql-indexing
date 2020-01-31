ad09a0c22b53be7130fa043875add49802ceac49
Jon Craton <jncraton@gmail.com>
2020-01-31 08:41:34 -0500

    Clean up queries

--- queries.sql
+++ queries.sql
@@ -1,29 +1,25 @@
-select "Total Rows";
-select count(1) from calls;
-
 .timer on
 
-select "Call counts by priority";
-
+-- Call counts by priority
 select Priority, count(1) 
   from calls
  group by Priority;
 
-select "Call counts by sector (location)";
 
+-- Call counts by sector (location)
 select Sector, count(1) as count 
   from calls 
  group by Sector 
  order by count desc;
 
-select "Description counts";
+
+-- Call counts by description
 select `Event Clearance Description`, count(1) as count 
   from calls 
  group by `Event Clearance Description` 
  order by count desc;
 
-select "Physical arrests made by sector";
-
+-- Physical arrests made by sector
 select Sector, count(1) as count 
   from calls 
  where `Event Clearance Description` = "PHYSICAL ARREST MADE" 
